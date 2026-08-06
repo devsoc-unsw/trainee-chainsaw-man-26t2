09fc6b1c170983dc64982cb7951ecf1f8f9e7578
Lucas <me@lhvy.dev>
2026-08-06 17:01:29 +1000

    feat: Add CI (#5)
    * Add CI
    
    * Fix CI path for server
    
    * Replace deprecated action
    
    * Fix Rust formatting
    
    * Move to pub(crate)

--- server/src/http/users.rs
+++ server/src/http/users.rs
@@ -2,7 +2,7 @@ use crate::http::ApiContext;
 use axum::Router;
 
 /// The router for all user routes.
-pub fn router() -> Router<ApiContext> {
+pub(super) fn router() -> Router<ApiContext> {
     // By having each module responsible for setting up its own routing,
     // it makes the root module a lot cleaner.
     Router::new()
