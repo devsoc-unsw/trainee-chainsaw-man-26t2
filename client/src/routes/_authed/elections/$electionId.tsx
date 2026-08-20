70a6138b988946660033ec7934b2c6f90889abd4
Lucas <me@lhvy.dev>
2026-08-20 10:48:17 +1000

    chore: Fix frontend formatting (#7)

--- client/src/routes/_authed/elections/$electionId.tsx
+++ client/src/routes/_authed/elections/$electionId.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authed/elections/$electionId')({
+export const Route = createFileRoute("/_authed/elections/$electionId")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/elections/$electionId"!</div>
+  return <div>Hello "/_authed/elections/$electionId"!</div>;
 }
--- client/src/routes/_authed/elections/new.tsx
+++ client/src/routes/_authed/elections/new.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_authed/elections/new')({
+export const Route = createFileRoute("/_authed/elections/new")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/elections/new"!</div>
+  return <div>Hello "/_authed/elections/new"!</div>;
 }
