66b8c2c36725f9151e266f65b1fb6d65ba83fb63
jiji-courier <kelly123huang@gmail.com>
2026-09-02 14:10:26 +1000

    Add touchups to roles page and layout

--- client/src/routes/_authed/elections/$electionId.tsx
+++ client/src/routes/_authed/elections/$electionId.tsx
@@ -7,8 +7,8 @@ export const Route = createFileRoute("/_authed/elections/$electionId")({
 function RouteComponent() {
   return (
     <div className="flex justify-center gap-16 p-8">
-      <div className="min-w-0 flex-1 max-w-2xl">
-        <Outlet />;
+      <div className="min-w-0 flex-1 max-w-4xl">
+        <Outlet />
       </div>
       {/* navigation bar */}
     </div>
