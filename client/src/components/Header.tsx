629126026f9720bdd99c1e4e55ae373c25511728
jiji-courier <kelly123huang@gmail.com>
2026-09-03 18:44:29 +1000

    Add UI for login page

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,7 @@ import { AvatarButton } from "./AvatarButton.tsx";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-10 flex items-center justify-between border-b border-on-dark/15 px-8 py-4 backdrop-blue-md bg-blue/15">
+    <header className="sticky top-0 z-10 flex items-center justify-between border-b border-on-dark/15 px-8 py-4 bg-blue/15">
       <DashboardButton />
       <div className="flex items-center gap-4">
         <NewElectionButton />
