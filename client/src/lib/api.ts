0718c595e46b902255787354a8683cd84a024798
jiji-courier <kelly123huang@gmail.com>
2026-09-06 01:40:07 +1000

    add roles api functions and types

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,40 @@
 import axios from "axios";
+import type {
+  CreateRoleRequest,
+  CreateRoleResponse,
+  Role,
+  UpdateRoleRequest,
+} from "@/lib/apiTypes";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
+
+export async function getRoles(campaignId: string): Promise<Array<Role>> {
+  const { data } = await api.get(`/campaigns/${campaignId}/roles`);
+  return data;
+}
+
+export async function createRole(
+  campaignId: string,
+  role: CreateRoleRequest,
+): Promise<CreateRoleResponse> {
+  const { data } = await api.post(`/campaigns/${campaignId}/roles`, role);
+  return data;
+}
+
+export async function updateRole(
+  campaignId: string,
+  roleId: string,
+  changes: UpdateRoleRequest,
+): Promise<void> {
+  await api.patch(`/campaigns/${campaignId}/roles/${roleId}`, changes);
+}
+
+export async function deleteRole(
+  campaignId: string,
+  roleId: string,
+): Promise<void> {
+  await api.delete(`/campaigns/${campaignId}/roles/${roleId}`);
+}
--- client/src/lib/apiTypes.ts
+++ client/src/lib/apiTypes.ts
@@ -1,6 +1,10 @@
 import type { components } from "./generated/schema";
 
+export type VotingCampaign = components["schemas"]["VotingCampaign"];
+export type VotingRole = components["schemas"]["VotingRole"];
+export type VotingCandidate = components["schemas"]["VotingCandidate"];
 
-export type VotingCampaign = components["schemas"]["VotingCampaign"]
-export type VotingRole = components["schemas"]["VotingRole"]
-export type VotingCandidate = components["schemas"]["VotingCandidate"]
+export type Role = components["schemas"]["Role"];
+export type CreateRoleRequest = components["schemas"]["CreateRoleRequest"];
+export type CreateRoleResponse = components["schemas"]["CreateRoleResponse"];
+export type UpdateRoleRequest = components["schemas"]["UpdateRoleRequest"];
