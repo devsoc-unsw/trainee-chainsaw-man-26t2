bb4edd269ecc50db738393b5519528fe0e979297
Chris <Citizn>
2026-09-06 15:52:31 +1000

    Updated API hooks

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,14 @@
+
+/**
+ * Get the backend URL for this deployment.
+ */
+export function backendUrl(): string {
+  return process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8080";
+}
+
+/**
+ * Get the frontend URL for this deployment.
+ */
+export function frontendUrl(): string {
+  return process.env.NEXT_PUBLIC_FRONTEND_URL ?? "http://localhost:3000";
+}
